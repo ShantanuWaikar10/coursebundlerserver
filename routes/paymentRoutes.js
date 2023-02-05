0a37d52b77370fbfa2819e612746bdcbbd94283b
ShantanuWaikar10 <89289274+ShantanuWaikar10@users.noreply.github.com>
2023-02-05 17:55:27 +0530

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { buySubscription, cancelSubscription, getRazorPayKey, paymentVerification } from "../controllers/paymentController.js";
-import { authorizeSubscribers, isAuthenticated } from "../middlewares/auth.js";
+import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
@@ -14,6 +14,6 @@ router.route("/paymentverification").post(isAuthenticated, paymentVerification);
 router.route("/razorpaykey").get(getRazorPayKey);
 
 // Cancel Subscription
-router.route("/subscribe/cancel").delete(isAuthenticated, authorizeSubscribers, cancelSubscription);
+router.route("/subscribe/cancel").delete(isAuthenticated, cancelSubscription);
 
 export default router;
